a81e4adc6183ebcd530d5359506e2e7b9b3f4ef4
Dan Aprahamian <dan.aprahamian@gmail.com>
2025-12-07 13:07:17 -0500

    template out the rest of the problems

---
+++ src/day5/mod.rs
@@ -0,0 +1,29 @@
+fn part1(_input: &Vec<&str>) -> u64 {
+    0
+}
+
+fn part2(_input: &Vec<&str>) -> u64 {
+    0
+}
+
+pub fn day5() {
+    let input_str = std::fs::read_to_string("src/day5/input.txt").expect("File should be present");
+    let input = input_str.trim().split("\n").collect::<Vec<_>>();
+    let result1 = part1(&input);
+    println!("Day 5 Part 1: {result1}");
+    let result2 = part2(&input);
+    println!("Day 5 Part 2: {result2}");
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_part_1() {
+        assert!(true)
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert!(true)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 mod day1;
+mod day10;
+mod day11;
+mod day12;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
 
 use std::collections::HashMap;
 
@@ -11,6 +19,14 @@ fn main() {
     days.insert(2, Box::new(day2::day2));
     days.insert(3, Box::new(day3::day3));
     days.insert(4, Box::new(day4::day4));
+    days.insert(5, Box::new(day5::day5));
+    days.insert(6, Box::new(day6::day6));
+    days.insert(7, Box::new(day7::day7));
+    days.insert(8, Box::new(day8::day8));
+    days.insert(9, Box::new(day9::day9));
+    days.insert(10, Box::new(day10::day10));
+    days.insert(11, Box::new(day11::day11));
+    days.insert(12, Box::new(day12::day12));
 
     let day_arg: Option<u64> = std::env::args().nth(1).and_then(|d| d.parse::<u64>().ok());
 
