fa784f07df83192427ede7374e38385e81ca0aa4
Dan Aprahamian <dan.aprahamian@gmail.com>
2025-12-07 14:09:12 -0500

    updating template for remaining items

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -1,14 +1,13 @@
-fn part1(_input: &Vec<&str>) -> u64 {
+fn part1(_input: &str) -> u64 {
     0
 }
 
-fn part2(_input: &Vec<&str>) -> u64 {
+fn part2(_input: &str) -> u64 {
     0
 }
 
 pub fn day7() {
-    let input_str = std::fs::read_to_string("src/day7/input.txt").expect("File should be present");
-    let input = input_str.trim().split("\n").collect::<Vec<_>>();
+    let input = std::fs::read_to_string("src/day7/input.txt").expect("File should be present");
     let result1 = part1(&input);
     println!("Day 7 Part 1: {result1}");
     let result2 = part2(&input);
@@ -17,13 +16,14 @@ pub fn day7() {
 
 #[cfg(test)]
 mod test {
+    const INPUT: &'static str = "";
     #[test]
     fn test_part_1() {
-        assert!(true)
+        assert_eq!(super::part1(INPUT), 0);
     }
 
     #[test]
     fn test_part_2() {
-        assert!(true)
+        assert_eq!(super::part2(INPUT), 0);
     }
 }
