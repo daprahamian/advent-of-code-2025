adcb7a237fbd4f859314889c769fc4b4f884b121
Dan Aprahamian <dan.aprahamian@gmail.com>
2025-12-07 14:09:56 -0500

    add inputs for part 6

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -16,10 +16,15 @@ pub fn day6() {
 
 #[cfg(test)]
 mod test {
-    const INPUT: &'static str = "";
+    const INPUT: &'static str = "\
+123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   + \
+";
     #[test]
     fn test_part_1() {
-        assert_eq!(super::part1(INPUT), 0);
+        assert_eq!(super::part1(INPUT), 4277556);
     }
 
     #[test]
